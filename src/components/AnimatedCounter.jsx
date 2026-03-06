9a46260d0a239e8bd08030ded19020cc572eea83
AYA-AMMI <ammiaya1502@gmail.com>
2026-03-06 13:55:51 +0000

    fixer le counter

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -8,10 +8,16 @@ const AnimatedCounter = () => {
             id="counter"
             className="padding-x-lg xl:mt-0 mt-32">
             <div className="mx-auto grid-4-cols">
-                {counterItems.map((item)=>(
-                    <div className="bg-zinc-700 rounded-lg p-10 flex flex-col justify-center">
-                        <div key={counterItems.label} className="counter-number text-white-50 text-5xl font-bold mb-2">
-                            <CountUp suffix={item.suffix} end={item.value}/>
+                {counterItems.map((item, index) => (
+                    <div key={index} className="bg-zinc-700 rounded-lg p-10 flex flex-col justify-center">
+                        <div className="counter-number text-white-50 text-5xl font-bold mb-2">
+                            <CountUp
+                                suffix={item.suffix}
+                                end={item.value}
+                                duration={2.5}
+                                enableScrollSpy={true}
+                                scrollSpyOnce={false}
+                            />
                         </div>
                         <div className="text-white-50 text-lg">{item.label}</div>
                     </div>
